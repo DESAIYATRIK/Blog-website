5fd9b422c161f63a80d03688929f40391fadf5cf
Yatrik Desai <yatrikdesai11@gmil.com>
2024-07-11 16:51:54 +0530

    v2.0.0

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
-
-const Connection = async (URL) => {
-    
+import dotenv from'dotenv';
+dotenv.config();
+const Connection = async () => {
+    const URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@blog-app.y00l5wc.mongodb.net/?retryWrites=true&w=majority&appName=blog-app`;
     try {
         await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Database connected successfully');
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
+import path from 'path';
 // Components
 import Connection from './database/db.js';
 import Router from './routes/route.js';
 
 dotenv.config();
-
+const __dirname=path.resolve();
 const app = express();
 const PORT = process.env.PORT || 8000;
 const USERNAME = process.env.DB_USERNAME;
@@ -31,12 +31,14 @@ app.use((req, res, next) => {
 // Routes
 app.use('/', Router);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static("client/build"));
-}
+app.use(express.static(path.join(__dirname,"./client/build")))
+app.get('*',function(_,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"),function(err){
+        res.status(500).send(err);
+    })
+})
 // Database connection
 
-const URL = process.env.MONGODB_URI || `mongodb+srv://${USERNAME}:${PASSWORD}@blog-app.y00l5wc.mongodb.net/?retryWrites=true&w=majority&appName=blog-app`;
-Connection(URL);
+Connection();
 // Start server
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
